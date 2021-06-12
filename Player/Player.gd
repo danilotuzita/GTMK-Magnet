6181a35c2854c49d78cb089acbf6412a8c64ce0f
raphaelgri <raphaelgriffo@hotmail.com>
2021-06-11 22:10:17 -0300

    Magnetic

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,12 +3,12 @@ extends KinematicBody2D
 const WALK_FORCE = 600
 const WALK_MAX_SPEED = 200
 const STOP_FORCE = 1300
-const JUMP_SPEED = 200
+const JUMP_SPEED = 10
 
 var velocity = Vector2()
 var other_player
 
-onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#nready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float):
 	# Horizontal movement code. First, get the player's input.
@@ -23,12 +23,25 @@ func _physics_process(delta: float):
 	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
 
 	# Vertical movement code. Apply gravity.
-	velocity.y += gravity * delta
+	#velocity.y += gravity * delta
+	
+	#Magnetic movement
+	
+	
+	#var magnetic_vector = position.move_toward(other_player.position,1)
+	var magnetic_vector =  other_player.position - position
+	
+	velocity += magnetic_vector*0.1
+	
+	$Line2D.points[1] = velocity
+	$Line2D2.points[1] = magnetic_vector
 
 	# Move based on the velocity and snap to the ground.
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-
+	
+	$Label.text = String(velocity)
+	
 	# Check for jumping. is_on_floor() must be called after movement code.
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -JUMP_SPEED
+	if (is_on_floor() or is_on_ceiling()) and Input.is_action_just_pressed("jump"):
+		velocity.y = -JUMP_SPEED*magnetic_vector.y
 		
